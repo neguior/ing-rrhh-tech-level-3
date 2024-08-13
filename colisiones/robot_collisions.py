b584cea949ca7973b78b7dd057925086810b7e16
NelsonGuio <nelsonguio@gmail.com>
2024-08-13 00:48:51 -0500

    Create robot_collisions.py
    Versión inicial

---
+++ colisiones/robot_collisions.py
@@ -0,0 +1,20 @@
+def robot_collisions(sequence: str) -> str:
+    n = len(sequence)
+    collisions = [0] * n
+    seq_list = list(sequence)
+
+    # Revisar colisiones iniciales
+    for i in range(n - 1):
+        if seq_list[i] == 'R' and seq_list[i + 1] == 'L':
+            collisions[i] += 1
+            collisions[i + 1] += 1
+            seq_list[i] = 'L'
+            seq_list[i + 1] = 'R'
+
+    # Revisar posibles colisiones subsiguientes
+    for i in range(n - 1):
+        if seq_list[i] == 'R' and seq_list[i + 1] == 'L':
+            collisions[i] += 1
+            collisions[i + 1] += 1
+
+    return ' '.join(map(str, collisions))
